29a121c0ca4b1bcb8267ae7dc0d63c5e6eaa8675
Davi Carrijo <davi.carrijo1@gmail.com>
2020-04-21 18:55:15 -0300

    News VFX + upgrade Universal RP

---
+++ BoardGame/Assets/0_VFX_Collection/_Scripts/ShieldReaction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldReaction : MonoBehaviour
+{
+    public GameObject ripplesVFX;
+
+    private Material mat;
+
+    private void OnCollisionEnter(Collision bateu)
+    {
+        var ripples = Instantiate(ripplesVFX, transform) as GameObject;
+        ripples.transform.localPosition = Vector3.zero;
+        var psr = ripples.GetComponent<ParticleSystemRenderer>();
+        mat = psr.material;
+        mat.SetVector("_SphereCenter", bateu.contacts[0].point);
+
+        //Destroy(ripples, 2);
+
+        /*if(bateu.gameObject.tag == "Enemy")
+        {
+            var ripples = Instantiate(ripplesVFX, transform) as GameObject;
+            var psr = ripples.transform.GetChild(0).GetComponent<ParticleSystemRenderer>();
+            mat = psr.material;
+            mat.SetVector("_SphereCenter", bateu.contacts[0].point);
+
+            Destroy(ripples, 2);
+        }*/
+    }
+}
---
+++ BoardGame/Assets/0_VFX_Collection/_Scripts/SpawnMeteor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMeteor : MonoBehaviour
+{
+    [SerializeField] public GameObject vfx;
+    [SerializeField] public Transform startPoint;
+    [SerializeField] public Transform endPoint;
+    void Start()
+    {
+        var startPos = startPoint.position;
+        GameObject objVFX = Instantiate(vfx, startPos, Quaternion.identity) as GameObject;
+
+        var endPos = endPoint.position;
+
+        RotateTo (objVFX, endPos);
+    }
+
+    void RotateTo(GameObject obj ,Vector3 destination)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+}
